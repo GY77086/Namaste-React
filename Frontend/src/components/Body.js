7368251f375a29aee6aa01e14958374651272a16
GY77086 <gy65905@gmail.com>
2026-06-13 18:50:11 +0530

    updating the fetch method

--- Frontend/src/components/Body.js
+++ Frontend/src/components/Body.js
@@ -33,6 +33,7 @@ const Body = () =>
 
     const fetchData = async () =>
     {
+        
         const data = await fetch ("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.4750346&lng=80.3532749&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");        
         // const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.7866747&lng=79.0037552&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
